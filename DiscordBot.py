820054930006f5e9afe349ff960ab7d03b0f1527
root <root@localhost.localdomain>
2018-03-03 22:35:54 -0500

    Updating the README.md. Removed the Token generated by the discord bot.

--- DiscordBot.py
+++ DiscordBot.py
@@ -4,14 +4,14 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import platform
 
-# Here you can modify the bot's prefix and description and wether it sends help in direct messages or not.
+# Here you can modify the bot's prefix and description and whether it sends help in direct messages or not.
+# command_prefix can be changed to have pretty much any special character
 client = Bot(description="Basic Bot by Ellimist#2651", command_prefix="!", pm_help = False)
 
 # Dictionary of trigger words that the bot will act upon
 triggerDict = {}
 
 # This is what happens everytime the bot launches. In this case, it prints information like server count, user count the bot is connected to, and the bot id in the console.
-# Do not mess with it because the bot can break, if you wish to do so, please consult me or someone trusted.
 @client.event
 async def on_ready():
 	print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
@@ -26,7 +26,7 @@ async def on_ready():
 	print('--------')
 	print('You are running BasicBot v2.1') #Do not change this. This will really help us support you, if you need support.
 	print('Created by Ellimist#2651')
-	return await client.change_presence(game=discord.Game(name='Online')) #This is buggy, let us know if it doesn't work.
+	return await client.change_presence(game=discord.Game(name='EllimistBot'))
 
 # This is a basic example of a call and response command. You tell it do "this" and it does it.
 # @client.command()
@@ -42,7 +42,7 @@ async def addTrigger(key, value):    # *args are used for untermined number of a
         """Adds a trigger to the triggerDict with the first message as key and the next as value"""
         await client.say("Add trigger")
         triggerDict[key] = value
-        await client.say('Trigger: {}, response: {}'.format(key, value))
+        await client.say(':white_check_mark: Trigger: {}, response: {}'.format(key, value))
         print(triggerDict)
 
 @client.command()
@@ -50,7 +50,7 @@ async def rmTrigger(key):
         """Removes from the triggerDict"""
         if key in triggerDict:
                 del triggerDict[key]
-                await client.say('Removed trigger: {}'.format(key))
+                await client.say(':warning: Removed trigger: {}'.format(key))
                 print(triggerDict)
 
 @client.event
@@ -63,7 +63,7 @@ async def on_message(message):
                                 await client.send_message(message.channel, triggerDict[word])
                                 return                
 	        
-client.run('NDE5NjY3MzgwNzc2NTk5NTUy.DXzdQA.bpiYB0TbccZzGhOs4k9r3UmzzD4')
+client.run('PUT YOUR BOT TOKEN HERE')
 
 # Basic Bot was created by Habchy#1665
 # Please join this Discord server if you need help: https://discord.gg/FNNNgqb
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# project_chat_bot
-Python chat bot that can be used in the Facebook Messenger
+# Project Chat Bot
+Python chat bot (discord.py) that can be invited to a Discord Server by the admin.
+
+Project was based on the this(https://github.com/Habchy/BasicBot/wiki) tutorial by Habchy.
+
+## Features:
+Bot can respond to user posts if the admin sets a specific trigger.
+
+The Bot searches every word of the post and responds with a custom response, setup by the admin,
+for whatever is the first trigger that it comes across.
+
+## Setting up a trigger:
+Anyone in the Discord Server can set up a custom Trigger and a Response.
+
+The user just need to use the set prefix (! for now) and use the addTrigger command.
+```
+Eg: !addTrigger "Trigger" "Response"
+```
+
+Similarly, the user can also remove Triggers using the rmTrigger command.
+```
+Eg: !rmTrigger "Trigger"
+```
